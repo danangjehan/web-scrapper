fbc22ad8877e5e195b1aec8ae9d198ea3c4cdbab
danang <danangjehan@outlook.com>
2021-10-04 11:43:22 +0700

    add comment

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const cheerio = require('cheerio')
 const app = express()
 const PORT = 5000
 
-const url = "https://www.jawapos.com/"
- 
-// app.get(url, function (req, res) {
-//     console.log(res)
-// })
+const url = "https://www.jawapos.com/" // url for web that you want to extract
 
 /**
  * iam using axios because it will executed immediately when script executed
@@ -19,6 +15,9 @@ axios(url).
         const article = [] // var for save scrapper result data
         const html = response.data
         const $ = cheerio.load(html)
+        /**
+         * in this case i am extrating trending news by extract data on class 'trending-item__title'
+         */
         $('.trending-item__title',html).each(function(){
             const text = $(this).text() // for title
             const url = $(this).find('a').attr('href') // for url
@@ -32,5 +31,5 @@ axios(url).
 
  
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log(`app listening at http://localhost:${PORT}`)
   })
